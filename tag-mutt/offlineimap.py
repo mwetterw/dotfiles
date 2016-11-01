226981e4f85831770746a17fb8068b40defdc9ee
Martin Wetterwald <martin@wetterwald.eu>
2016-11-02 00:44:39 +0100

    Add a global GMail nametrans in offlineimap.py

--- tag-mutt/offlineimap.py
+++ tag-mutt/offlineimap.py
@@ -3,3 +3,24 @@ from subprocess import check_output
 
 def fetch_pass(account):
     return check_output("pass Mail/" + account, shell=True).splitlines()[0]
+
+nametrans_google_in = {
+    '[Gmail]/All Mail':     'All',
+    '[Gmail]/Chats':        'Chats',
+    '[Gmail]/Drafts':       'Drafts',
+    '[Gmail]/Sent Mail':    'Sent',
+    '[Gmail]/Important':    'Important',
+    '[Gmail]/Spam':         'Spam',
+    '[Gmail]/Starred':      'Starred',
+    '[Gmail]/Trash':        'Trash'
+}
+
+nametrans_google_out = {
+    v: k for k, v in nametrans_google_in.iteritems()
+}
+
+folderfilter_google = lambda folder: folder not in [
+                                                '[Gmail]/All Mail',
+                                                '[Gmail]/Important',
+                                                '[Gmail]/Starred'
+                                                ]
