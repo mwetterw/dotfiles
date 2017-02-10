478ad65e6987905cb78743b2fb203b38f94e6041
Martin Wetterwald <martin@wetterwald.eu>
2017-02-10 11:03:18 +0100

    [mutt] Add autoconf as dependency when installing

--- install_neomutt.sh
+++ install_neomutt.sh
@@ -3,8 +3,8 @@ set -e
 
 git clone https://github.com/neomutt/neomutt.git --depth 1 -b neomutt-20161028
 cd neomutt
+sudo apt-get install autoconf libnotmuch-dev liblmdb-dev libssl-dev lynx xsltproc urlview links msmtp libncursesw5-dev offlineimap
 autoreconf --install
-sudo apt-get install libnotmuch-dev liblmdb-dev libssl-dev lynx xsltproc urlview links msmtp libncursesw5-dev offlineimap
 ./configure --enable-sidebar --enable-notmuch --enable-hcache --enable-imap --with-ssl --enable-locales-fix
 make
 sudo make install
