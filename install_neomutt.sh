7ae123c128590529584a407a1b6bbb31b2db73ef
Martin Wetterwald <martin@wetterwald.eu>
2017-10-30 18:57:20 +0100

    [mutt] Upgrade to newest version

--- install_neomutt.sh
+++ install_neomutt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/neomutt/neomutt.git --depth 1 -b neomutt-20161028
+git clone https://github.com/neomutt/neomutt.git --depth 1 -b neomutt-20171027
 cd neomutt
-sudo apt-get install autoconf libnotmuch-dev liblmdb-dev libssl-dev lynx xsltproc urlview links msmtp libncursesw5-dev offlineimap
+sudo apt-get -y install autoconf autopoint libnotmuch-dev liblmdb-dev libssl-dev lynx xsltproc urlview links msmtp libncursesw5-dev offlineimap libtokyocabinet-dev
 autoreconf --install
-./configure --enable-sidebar --enable-notmuch --enable-hcache --enable-imap --with-ssl --enable-locales-fix
+./configure --enable-notmuch --enable-locales-fix --with-tokyocabinet --with-ssl
 make
 sudo make install
 cd ..
