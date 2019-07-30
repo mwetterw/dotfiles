842fa90fda634575e438c7ba61cf1ab8ccbd976c
Martin Wetterwald <martin@wetterwald.eu>
2019-07-30 14:50:22 +0200

    [urxvt] Switch back to standalone urxvt (21e34f2)

--- tag-awesome4.0/config/awesome/rc.lua
+++ tag-awesome4.0/config/awesome/rc.lua
@@ -47,7 +47,7 @@ beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 
-terminal = "urxvtc"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "editor"
 
 editor_cmd = terminal .. " -e " .. editor
